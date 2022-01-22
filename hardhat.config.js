aedfdc33132010b01edee7f4e0d137db0321ac3c
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-23 07:18:30 +0800

    Removed module.exports

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,3 @@
 require('@nomiclabs/hardhat-waffle');
 require('dotenv').config();
 
-module.exports = {
-    solidity: "0.8.0",
-    networks: {
-        rinkeby: {
-            url: `${process.env.ALCHEMY.RINKEBY.URL}`,
-            accounts: [`0x${process.env.RINKEBY_PRIVATE_KEY}`],
-        }
-    }
-};
