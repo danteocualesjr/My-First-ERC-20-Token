95fc2b8b4148da0c36e65a0c5f703e99940bd9b3
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-23 09:27:45 +0800

    Renamed Hobbit to Cuales

--- contracts/Cuales.sol
+++ contracts/Cuales.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Hobbit is ERC20 {
+contract Cuales is ERC20 {
     uint constant _initial_supply = 100 * (10*18);
-    constructor() ERC20("Hobbit", "H") public {
+    constructor() ERC20("Cuales", "CT") public {
         _mint(msg.sender, _initial_supply)
     }
 }
+
