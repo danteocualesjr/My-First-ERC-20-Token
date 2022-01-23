22925099f2d36641fb4fb39d9d6c50f255c4f52c
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-23 10:23:17 +0800

    Renamed Cuales to Tolken

--- contracts/Tolken.sol
+++ contracts/Tolken.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Cuales is ERC20 {
+contract Tolken is ERC20 {
     uint constant _initial_supply = 100 * (10**18);
-    constructor() ERC20("Cuales", "CT") public {
+    constructor() ERC20("Tolken", "TT") public {
         _mint(msg.sender, _initial_supply)
-    }
+    };
 }
 
